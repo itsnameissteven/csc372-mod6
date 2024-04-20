449ac1486d0facbb200093d7e0fe4222dfa1cb79
Steven Mancine <steven.mancine@charter.com>
2024-04-20 14:59:28 -0600

    Add student class

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+  private int rollno;
+  private String name, address;
+
+  Student(int rollno, String name, String address){
+    this.rollno = rollno;
+    this.name = name;
+    this.address = address;
+  }
+
+  // Getters
+  public int getRollno() {return this.rollno;};
+  public String getName() {return this.name;};
+  public String getAddress() {return this.address;};
+
+  // Setters 
+  public void setRollno(int rollno) {
+    this.rollno = rollno;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public void setAddress(String address) {
+    this.address = address;
+  }
+}
