3474dc3197f36e87093f2455466c7600771a536a
Steven Mancine <steven.mancine@charter.com>
2024-04-21 19:51:33 -0600

    SAve

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello, World!");
-}
+      ArrayList<Student> students = new ArrayList<>();
+      RollnoComparator rollnoComparator = new RollnoComparator();
+      MergeSort<Student> mergeSort = new MergeSort<Student>(rollnoComparator);
+
+      students.add(new Student(7, "David Lee", "741 Birch St"));
+      students.add(new Student(1, "John Doe", "123 Main St"));
+      students.add(new Student(4, "Emily Davis", "321 Pine St"));
+      students.add(new Student(5, "Michael Wilson", "654 Maple St"));
+      students.add(new Student(8, "Emma Taylor", "852 Walnut St"));
+      students.add(new Student(10, "Olivia White", "147 Cherry St"));
+      students.add(new Student(3, "Bob Johnson", "789 Oak St"));
+      students.add(new Student(6, "Sarah Brown", "987 Cedar St"));
+      students.add(new Student(2, "Alice Smith", "456 Elm St"));
+      students.add(new Student(9, "James Clark", "369 Laurel St"));
+
+      mergeSort.splitArray(students, 0, students.size());
+
+      for(Student student : students) {
+        System.out.println(student.getRollno());
+      }
+
+  }
 }
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
-public class MergeSort {
-  ArrayList<Integer> numbers;
-  public void splitArray(ArrayList<Integer> numbers, int startingPoint, int endingPoint) {
+import java.util.Comparator;
+
+public class MergeSort <T> {
+  Comparator<T> comparer;
+  
+  MergeSort(Comparator<T> comparer) {
+    this.comparer = comparer;
+  }
+  public void splitArray(ArrayList<T> data, int startingPoint, int endingPoint) {
     int midPoint;
 
     // Only run if pointers do not cross paths
@@ -9,30 +15,30 @@ public class MergeSort {
       midPoint = (startingPoint + endingPoint) / 2;
 
       // Split the left side
-      splitArray(numbers, startingPoint, midPoint);
+      splitArray(data, startingPoint, midPoint);
       // Split the right side
-      splitArray(numbers, midPoint + 1, endingPoint);
+      splitArray(data, midPoint + 1, endingPoint);
 
-      mergeData(numbers, startingPoint, midPoint, endingPoint);
+      mergeData(data, startingPoint, midPoint, endingPoint);
     }
   }
 
-  public static void mergeData(ArrayList<Integer> numbers, int startingPoint, int midPoint, int endingPoint) {
+  public void mergeData(ArrayList<T> data, int startingPoint, int midPoint, int endingPoint) {
     int tempSize = endingPoint - startingPoint + 1; // 
-    int[] tempArray = new int[tempSize];
+    ArrayList<T> tempArray = new ArrayList<T>(tempSize);
     int mergePos, leftPointer, rightPointer;
 
     mergePos = 0;
     leftPointer = startingPoint;
     rightPointer = endingPoint + 1;
-
+    
     // get the smallest element from each list, compare and insert into temp array.
     while (leftPointer <= midPoint && rightPointer <= endingPoint) {
-      if (numbers.indexOf(leftPointer) < numbers.indexOf(rightPointer)) {
-        tempArray[mergePos] = numbers.indexOf(leftPointer);
+      if (this.comparer.compare(data.get(leftPointer), data.get(rightPointer)) == -1) {
+        tempArray.set(mergePos, data.get(leftPointer)) ;
         leftPointer++;
       } else {
-        tempArray[mergePos] = numbers.indexOf(rightPointer);
+        tempArray.set(mergePos, data.get(rightPointer));
         rightPointer++;
       }
       mergePos++;
@@ -40,21 +46,22 @@ public class MergeSort {
 
     // Add remainder of first array to temp array
     while(leftPointer <= midPoint) {
-      tempArray[mergePos] = numbers.indexOf(leftPointer);
+      tempArray.set(mergePos, data.get(leftPointer));
       leftPointer++;
       mergePos++;
     }
 
     // Add remainder of second array to temp array
     while(rightPointer <= endingPoint) {
-      tempArray[mergePos] = numbers.indexOf(rightPointer);
+      tempArray.set(mergePos, data.get(rightPointer));
       rightPointer++;
       mergePos++;
     }
 
     // Use temp array to copy back into original arrayList
     for(mergePos = 0; mergePos < tempSize; mergePos++) {
-      numbers.set(startingPoint + mergePos, tempArray[mergePos]);
+      System.out.println(mergePos);
+      data.set(startingPoint + mergePos, tempArray.get(mergePos));
     }
   }
 }
